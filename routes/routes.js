29c7b8c225bea8aef31888287a671b8282027c58
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-05-07 17:20:32 -0500

    update for cors

--- routes/routes.js
+++ routes/routes.js
@@ -1,55 +1,57 @@
 const express = require('express');
-const cors = require('cors');
+
 const router = express.Router()
+const app = express();
+const cors = require('cors');
 const State = {
     states: require('../models/states.json'),
     setStates: function (data) { this.states = data }
 }
 const Funfact = require('../models/Funfacts');
 const statesController = require('../controllers/statesController');
-app.use(cors());
 
+app.use(cors());
 module.exports = router;
 
 
 
 router.route('/states')
-    .get(statesController.getAllStates)
+    .get(cors(),statesController.getAllStates)
     
 //Working
 
     
 
 router.route('/states/:state')
-    .get(statesController.getOneState)
+    .get(cors(),statesController.getOneState)
 
 router.route('/states/:state/capital')
-    .get(statesController.getCapital);
+    .get(cors(),statesController.getCapital);
 
 router.route('/states/:state/nickname')
-    .get(statesController.getNickname);
+    .get(cors(),statesController.getNickname);
 
 router.route('/states/:state/population')
-    .get(statesController.getPopulation);
+    .get(cors(),statesController.getPopulation);
 
 router.route('/states/:state/admission')
-    .get(statesController.getAdmission);
+    .get(cors(),statesController.getAdmission);
 
 
 router.route('/states/:state/funfact')
-    .get(statesController.getFunFacts)
+    .get(cors(),statesController.getFunFacts)
 router.route('/states/:state/funfact')
     
-    .post(statesController.postFunFacts)
-    .patch(statesController.updateFunFact);
+    .post(cors(),statesController.postFunFacts)
+    .patch(cors(),statesController.updateFunFact);
 
 
 //Update Funfact Method
-router.patch('/states/:state/funfact', (req, res) => {
+router.patch('/states/:state/funfact', cors(),(req, res) => {
     res.send('Update by ID API')
 })
 
 //Delete Funfact Method
-router.delete('/states/:state/funfact', (req, res) => {
+router.delete('/states/:state/funfact',cors(), (req, res) => {
     res.send('Delete by ID API')
 })
